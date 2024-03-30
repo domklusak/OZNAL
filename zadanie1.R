f6573c78c75da9e3855ca7df46697bcf91371fab
dominikklusak <dom.klusak@gmail.com>
2024-03-30 18:41:50 +0100

    Added train model for regression, evaluted model, classification, results

--- zadanie1.R
+++ zadanie1.R
@@ -1,4 +1,3 @@
-# Load the Tidyverse, a collection of R packages for data science
 library(tidyverse)
 
 # Load the dataset
@@ -37,3 +36,32 @@ ggplot(matches, aes(x = away_score)) + geom_histogram(binwidth = 1, fill = "red"
 # Outcome distribution
 ggplot(matches, aes(x = outcome)) + geom_bar(fill = "green") +
   ggtitle("Match Outcome Distribution") + xlab("Outcome") + ylab("Count")
+
+
+# Train a linear regression model
+lm_model <- lm(home_score ~ away_score + neutral + factor(tournament), data = matches)
+
+library(nnet)
+multinom_model <- multinom(outcome ~ away_score + neutral + factor(tournament), data = matches)
+
+
+# Calculate predictions and evaluate the model
+# For regression
+predicted_scores <- predict(lm_model, type = "response")
+MAE <- mean(abs(predicted_scores - matches$home_score))
+RMSE <- sqrt(mean((predicted_scores - matches$home_score)^2))
+
+# For classification
+library(caret)
+
+# Convert both predicted outcomes and actual outcomes to factors with the same levels
+predicted_outcomes_factor <- factor(predicted_outcomes, levels = c("win", "lose", "draw"))
+matches$outcome_factor <- factor(matches$outcome, levels = c("win", "lose", "draw"))
+
+#Confusion matrix
+confusion_matrix_result <- confusionMatrix(data = predicted_outcomes_factor, reference = matches$outcome_factor)
+
+#Results
+print(confusion_matrix_result)
+
+
