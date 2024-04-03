1f314e442785e89ce67cf464a9561647c99d0ea2
dominikklusak <dom.klusak@gmail.com>
2024-04-03 21:38:45 +0200

    feat: added boxplot for outlayers, Implemented ROC calculation

--- zadanie1.R
+++ zadanie1.R
@@ -42,6 +42,14 @@ ggplot(matches, aes(x = away_score)) + geom_histogram(binwidth = 1, fill = "red"
 ggplot(matches, aes(x = outcome)) + geom_bar(fill = "green") +
   ggtitle("Match Outcome Distribution") + xlab("Outcome") + ylab("Count")
 
+#boxplots for outlayers
+ggplot(matches, aes(y = home_score)) + geom_boxplot() + 
+  ggtitle("Boxplot of Home Scores") + ylab("Home Score")
+
+ggplot(matches, aes(y = away_score)) + geom_boxplot() + 
+  ggtitle("Boxplot of Away Scores") + ylab("Away Score")
+
+
 
 # Train a linear regression model
 lm_model <- lm(home_score ~ away_score + neutral + factor(tournament), data = matches)
@@ -72,4 +80,23 @@ confusion_matrix_result <- confusionMatrix(data = predicted_outcomes_factor, ref
 #Results
 print(confusion_matrix_result)
 
+library(pROC)
 
+# First, we get the predicted probabilities for the "win" classification
+predicted_probs <- predict(multinom_model, newdata = matches, type = "prob")
+
+# Now, we convert the actual outcomes and the predicted probabilities to a binary form for the "win" class
+matches$binary_outcome <- as.numeric(matches$outcome == "win")
+predicted_probs_win <- predicted_probs[,"win"]
+
+# We create the ROC object for the "win" class
+roc_curve <- roc(matches$binary_outcome, predicted_probs_win)
+
+# We calculate the AUC for the ROC curve
+auc_value <- auc(roc_curve)
+
+# We plot the ROC curve
+plot(roc_curve, main = paste("ROC Curve for 'win' class, AUC =", auc_value))
+
+# We print the AUC value
+print(auc_value)
